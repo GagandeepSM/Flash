5dae01a6e4d82e95f017c6c92481b461258ff04b
Gagandeep Manku <id4gsm@gmail.com>
2019-09-27 14:52:41 +0530

    Add files via upload

---
+++ java/Friends.java
@@ -0,0 +1,22 @@
+package com.flashmessaging.chat.flash;
+
+public class Friends {
+
+    public String date;
+
+    public Friends(){
+
+    }
+
+    public Friends(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
